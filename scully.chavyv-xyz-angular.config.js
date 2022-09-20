74e5a5cab9f438279486ad998bfb98aea74dcb78
Habibullah Akbar <akbar2habibullah@gmail.com>
2022-09-21 04:33:29 +0700

    Modify text contrast skills section

---
+++ scully.chavyv-xyz-angular.config.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+require("@scullyio/scully-plugin-playwright");
+exports.config = {
+    projectRoot: './src',
+    projectName: 'chavyv-xyz-angular',
+    distFolder: './dist/chavyv-xyz-angular',
+    outDir: './dist/static',
+    defaultPostRenderers: [],
+    routes: {},
+};
