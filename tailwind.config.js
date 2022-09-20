cfe9f748b67365307752d5f0970f197ae6499eb3
Habibullah Akbar <akbar2habibullah@gmail.com>
2022-09-21 02:35:45 +0700

    Try to use Github Actions for build

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,17 @@ module.exports = {
   important: true,
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {
-      colors: {
-        "main-dark-color": "#1a535c",
-        "main-secondary-color": "#cbf3f0",
-        "main-light-color": "#f7fff7",
-        "secondary-dark-color": "#fb7c33",
-        "secondary-light-color": "#ffe66d",
-        "dark-charcoal": "#333",
-        "dark-shade-gray": "#444",
-        transparent: "transparent",
-      },
+    colors: {
+      "main-dark-color": "#1a535c",
+      "main-secondary-color": "#cbf3f0",
+      "main-light-color": "#f7fff7",
+      "secondary-dark-color": "#fb7c33",
+      "secondary-light-color": "#ffe66d",
+      "dark-charcoal": "#333",
+      "dark-shade-gray": "#444",
+      transparent: "transparent",
+      white: "white",
+      black: "black",
     },
   },
   plugins: [],
