3370d3dab108aa98db89b31a8b2343892ee7e4ec
Habibullah Akbar <akbar2habibullah@gmail.com>
2022-09-24 15:16:56 +0700

    Add img-src permission

--- customBuildReducer.mjs
+++ customBuildReducer.mjs
@@ -175,7 +175,7 @@ function removeScriptCall() {
     </script>
   `;
 
-  const csp = `<meta http-equiv="Content-Security-Policy" content="default-src 'self' 'unsafe-inline'">`;
+  const csp = `<meta http-equiv="Content-Security-Policy" content="default-src 'self' 'unsafe-inline' 'img-src'">`;
 
   readFile("dist/static/index.html", function (err, data) {
     const new_html = new String(data)
